e6812b26aa5ae57c8536256f6935f50210ee13cb
gustavolmarquesg4f-dotcom <gustavolmarquesg4f-dotcom@users.noreply.github.com>
2026-08-21 02:15:59 +0000

    deploy: b93a2fd71421292cef8b6810b913ce3f955c1e00

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "biblia-em-visao-offline-v3";
+const CACHE_NAME = "biblia-em-visao-offline-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
